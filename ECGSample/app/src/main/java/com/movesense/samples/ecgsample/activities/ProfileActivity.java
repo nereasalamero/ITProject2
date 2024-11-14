aabc3b14d2a9e0ebaafdce432326bd4f1449a64f
Nerea Salamero <nereasalamero@gmail.com>
2024-11-14 09:50:29 +0200

    Merging DocSense and ECGSample

---
+++ ECGSample/app/src/main/java/com/movesense/samples/ecgsample/activities/ProfileActivity.java
@@ -0,0 +1,28 @@
+package com.movesense.sample.docsense;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+    private TextView welcomeText;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        welcomeText = findViewById(R.id.welcome_text);
+
+        // Retrieve the username from the Intent
+        String username = getIntent().getStringExtra("USERNAME");
+
+        // Display the welcome message with the username
+        if (username != null) {
+            welcomeText.setText("Welcome " + username + "to your profile !");
+        }
+    }
+}
+
